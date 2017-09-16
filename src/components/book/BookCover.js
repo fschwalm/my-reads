332d3bb3a5ef925c8bc860612ea191b8903a95d7
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2017-09-16 03:31:50 -0300

    feat: Create BookCover component

---
+++ src/components/book/BookCover.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function BookCover(props) {
+  // TODO: Check which one is better
+  /*<img src={props.image + '.jpg'} width="128" height="193" alt="" />*/
+  return (
+    <div
+      className="book-cover"
+      style={{ width: 128, height: 193, backgroundImage: `url(${props.image})` }}>
+    </div>
+  );
+}
+
+export default BookCover;
