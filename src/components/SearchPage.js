8b4fdb920ddac7e20329d823385477b41cfd68f1
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2017-09-18 22:47:39 -0300

    refactor: Search page to a controlled component

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,29 +1,50 @@
 import React from 'react';
+import { Debounce } from 'react-throttle';
 import { Link } from 'react-router-dom';
+import BookGrid from './book/BookGrid';
+import * as BooksAPI from '../BooksAPI';
 
-function SearchPage() {
+class SearchPage extends React.Component {
+  state = {
+    query: '',
+    results: []
+  };
+
+  constructor() {
+    super();
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch(event) {
+    let value = event.target.value;
+    // TODO: Verificar o motivo de chamar conforme o número de letras.
+    BooksAPI.search(value, 20).then((results) => {
+      if(results.error) {
+        this.setState({ results: [] });
+      } else {
+        this.setState({ results });
+      }
+      console.log(results);
+    });
+  }
+
+  render() {
   return (
     <div className="search-books">
       <div className="search-books-bar">
         <Link className="close-search" to="/">Close</Link>
         <div className="search-books-input-wrapper">
-          {/*
-            NOTES: The search from BooksAPI is limited to a particular set of search terms.
-            You can find these search terms here:
-            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-            you don't find a specific author or title. Every search is limited by search terms.
-          */}
-          <input type="text" placeholder="Search by title or author"/>
-
+        <Debounce time="800" handler="onChange">
+          <input type="text" onKeyUp={this.handleSearch} placeholder="Search by title or author"/>
+        </Debounce>
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <BookGrid books={this.state.results} />
       </div>
     </div>
-  )
+    )
+  }
 }
 
 export default SearchPage;
