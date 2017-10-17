61cfaaccd0df41d49ed085801886a6f6ac91adac
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2017-10-17 01:25:10 -0200

    feat: Status messages on SearchPage
    While data is fetched is displayed a Loading... status message.
    If the search does not bring result a message is displayed.

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -2,27 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import BookGrid from './bookgrid/BookGrid';
 import SearchBar from './search-bar/SearchBar';
+import BookModel from '../model/BookModel';
 
 const propTypes = {
-  searchResult: PropTypes.arrayOf(PropTypes.object),
+  searchResult: PropTypes.shape({
+    books: PropTypes.arrayOf(PropTypes.instanceOf(BookModel)),
+    query: PropTypes.any,
+    hasError: PropTypes.boolean,
+  }),
   onSearch: PropTypes.func.isRequired,
   onUpdateBook: PropTypes.func.isRequired,
 };
 
 const defaultProps = {
-  searchResult: [],
+  searchResult: {
+    books: [],
+    query: '',
+    hasError: false,
+  },
 };
 
 function SearchPage({ searchResult, onSearch, onUpdateBook }) {
+  const { books, query, hasError } = searchResult;
+  const isDefaultState = query === '' && books.length === 0 && hasError === false;
+  const noResultForQuery = query !== '' && books.length === 0 && hasError;
+
   return (
     <div className="search-books">
       <SearchBar onSearch={onSearch} />
-      {searchResult.length > 0 &&
       <div className="search-books-results">
-        {/* TODO: show message to empty results */}
-        <BookGrid onUpdateBook={onUpdateBook} books={searchResult} />
+        {books.length > 0 && <BookGrid onUpdateBook={onUpdateBook} books={books} />}
+        {isDefaultState ? null : (
+          <div>
+            {noResultForQuery ? (
+              <p className="books-grid">No results for {query}</p>
+            ) : (
+              <p className="books-grid">Loading...</p>
+            )}
+          </div>
+        )}
       </div>
-      }
     </div>
   );
 }
