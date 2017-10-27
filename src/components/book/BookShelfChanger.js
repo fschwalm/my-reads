464cf702c11038f334325e2d6a07a0f90ece962d
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2017-10-26 22:03:19 -0200

    feat: Add loading spinner while book is updating

--- src/components/book/BookShelfChanger.js
+++ src/components/book/BookShelfChanger.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { BounceLoader } from 'react-spinners';
 
 const propTypes = {
   currentShelf: PropTypes.oneOf(['currentlyReading', 'wantToRead', 'read', 'none', 'moveTo']),
@@ -15,30 +16,41 @@ class BookShelfChanger extends React.Component {
     super(props);
     this.state = {
       currentShelf: props.currentShelf,
+      isUpdating: false,
     };
     this.handleShelfUpdate = this.handleShelfUpdate.bind(this);
     this.update = this.update.bind(this);
   }
   handleShelfUpdate(event) {
+    this.setState({ isUpdating: true });
     this.props.onUpdateShelf(event, this.update);
   }
 
   update(shelf) {
-    this.setState({ currentShelf: shelf });
+    this.setState({ currentShelf: shelf, isUpdating: false });
   }
 
   render() {
+    const { isUpdating, currentShelf } = this.state;
     return (
-      <div className="book-shelf-changer">
-        <select value={this.state.currentShelf} onChange={this.handleShelfUpdate}>
-          <option value="moveTo" disabled>
-            Move to...
-          </option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
+      <div>
+        {isUpdating ? (
+          <div className="book-shelf-loading">
+            <BounceLoader size={50} color="#5ac8fa" loading />
+          </div>
+        ) : (
+          <div className="book-shelf-changer">
+            <select value={currentShelf} onChange={this.handleShelfUpdate}>
+              <option value="moveTo" disabled>
+                Move to...
+              </option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        )}
       </div>
     );
   }
