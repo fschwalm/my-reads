cf060b1cb556bb62ff144743e71fdbf1acda567d
Fagner <fagnerschwalm@gmail.com>
2017-10-31 17:33:55 -0200

    test: Add polyfills to remove warnings

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,16 +1,18 @@
+import './polyfills';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
 /**
  * http://airbnb.io/enzyme/docs/installation/index.html#working-with-react-16
  */
 configure({ adapter: new Adapter() });
 
-/**
-* https://github.com/facebookincubator/create-react-app/issues/3199
-*/
-global.requestAnimationFrame = function (callback) {
-  setTimeout(callback, 0);
-};
+// Make Enzyme functions available in all test files without importing
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+global.toJson = toJson;
 
 /**
  * https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#initializing-test-environment
